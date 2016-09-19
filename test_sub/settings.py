c1b6b09574e4c72e221608c34da794e03de92b58
smriti <iamsmritid@gmail.com>
2016-09-19 18:12:32 +0530

    added registration page

--- test_sub/settings.py
+++ test_sub/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 SERIALIZABLE_VALUE={
 
 }
+
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/sta  tic".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static/'),
+ )
