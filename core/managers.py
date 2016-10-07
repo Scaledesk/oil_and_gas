e2897c6d85322d106c61cee32bfe9627f9e2611b
tushar_scaledesk <tushar@scaledesk.com>
2016-10-07 14:45:28 +0530

    add Company model manager

---
+++ core/managers.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import User
+
+from django.db.models import Manager
+
+
+class CompanyModelManager(Manager):
+    def create_company(self, **kaargs):
+        user = None
+        try:
+            user = User.objects.get(id=kaargs.get("user_id"))
+        except User.DoesNotExist:
+            user = User.objects.create_user(kaargs.get("company_email"), kaargs.get("company_email"),
+                                            kaargs.get("password"))
+            user.first_name = kaargs.get("username").split()[0]
+            if kaargs.get("username").split()[1]:
+                user.last_name = kaargs.get("username").split()[1]
+            user.save()
+            # return  False, "User Does Not exist", None
+        company = self.create(company_name=kaargs.get("company_name"), email=kaargs.get("company_email"),
+                              ad_reference=kaargs.get("ad_reference"),
+                              owner=user)
+        return True, "Company Created Successfully", company
